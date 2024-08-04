6bd91c83364e353c5619e1cf7fef09364c07d567
Nick <nkmitkov@gmail.com>
2024-08-04 17:22:43 +0300

    Fix edit functionality
    I make edit modal with uncontrolled form and basic validation after Submit

--- client/src/components/DetailsPage/EditAttractionModal.jsx
+++ client/src/components/DetailsPage/EditAttractionModal.jsx
@@ -2,7 +2,9 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
+import UserError from '../UserError/UserError';
 
 export default function EditAttractionModal({
     show,
@@ -10,39 +12,41 @@ export default function EditAttractionModal({
     onEditHandler,
     attraction,
 }) {
-    const [formValues, setFormValues] = useState({});
+    const [userErrorMessage, setUserErrorMessage] = useState("");
 
-    useEffect(() => {
-        setFormValues(attraction);
-    }, []);
 
-    const onChangeHandler = (e) => {
-        setFormValues(state => ({
-            ...state,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    const resetFormHandler = (isClear) => {
-        if (isClear) {
-            return setFormValues({
-                name: "",
-                image: "",
-                address: "",
-                hours: "",
-                phone: "",
-                price: "",
-                website: "",
-                description: "",
-            });
-        }
-        setFormValues(attraction)
-    };
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
 
-        onEditHandler(formValues);
+        const formData = new FormData(e.currentTarget);
+        const { name,
+            image,
+            address,
+            hours,
+            phone,
+            price,
+            website,
+            description } = Object.fromEntries(formData);
+
+        if (!name || !image || !address || !hours || !phone || !price || !website || !description) {
+            setTimeout(() => {
+                setUserErrorMessage("");
+            }, 2500);
+
+            return setUserErrorMessage("All input fields are required");
+        };
+
+        onEditHandler({
+            name,
+            image,
+            address,
+            hours,
+            phone,
+            price,
+            website,
+            description
+        });
     };
 
     return (
@@ -51,39 +55,40 @@ export default function EditAttractionModal({
                 <Modal.Title>Edit Attraction</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
+                <Form onSubmit={onSubmitHandler}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="name" placeholder="Attraction's name.." value={formValues.name} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="name" placeholder="Attraction's name.." defaultValue={attraction.name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="image" placeholder="Attraction's image URL.." value={formValues.image} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="image" placeholder="Attraction's image URL.." defaultValue={attraction.image} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="address" placeholder="Attraction's address.." value={formValues.address} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="address" placeholder="Attraction's address.." defaultValue={attraction.address} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="hours" placeholder="Attraction's operating hours.." value={formValues.hours} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="hours" placeholder="Attraction's operating hours.." defaultValue={attraction.hours} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="phone" placeholder="Attraction's phone.." value={formValues.phone} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="phone" placeholder="Attraction's phone.." defaultValue={attraction.phone} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="price" placeholder="Attraction's entrance fee.." value={formValues.price} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="price" placeholder="Attraction's entrance fee.." defaultValue={attraction.price} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="website" placeholder="Attraction's website URL.." value={formValues.website} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="website" placeholder="Attraction's website URL.." defaultValue={attraction.website} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="text" name="description" placeholder="Attraction's short description.." value={formValues.description} onChange={onChangeHandler} />
+                        <Form.Control type="text" name="description" placeholder="Attraction's short description.." defaultValue={attraction.description} />
                     </Form.Group>
+
+                    {userErrorMessage && <UserError message={userErrorMessage} />}
+
+                    <Modal.Footer>
+                        <Button variant="primary" type="submit">Save</Button>
+                        <Button variant="secondary" onClick={onEditModalClose}>Close</Button>
+                    </Modal.Footer>
                 </Form>
             </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={onSubmitHandler}>Save</Button>
-                <Button variant="secondary" onClick={(e) => resetFormHandler()}>Reset</Button>
-                <Button variant="secondary" onClick={(e) => resetFormHandler(true)}>Clear</Button>
-                <Button variant="secondary" onClick={onEditModalClose}>Close</Button>
-            </Modal.Footer>
         </Modal>
     );
 }
